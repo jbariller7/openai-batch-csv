c0507e449b68a3feca98f20d25e9be3807d1b9d3
jbariller7 <116109740+jbariller7@users.noreply.github.com>
2025-09-08 09:48:50 +0200

    Update ping.js

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,5 +1,3 @@
-// netlify/functions/ping.js
-export const config = { path: "/api/ping" };
 export default async () =>
   new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
     status: 200,
